ec6c0c044dcfc7eb157191965dfafa22742336b4
Mr. Chirag <160012530+ChiragAg24@users.noreply.github.com>
2026-04-02 15:33:55 +0530

    Add files via upload

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class BinarySearch{
+    public static int BinaryS(int[] arr, int target){
+        int left = 0;
+        int right = arr.Length-1;
+
+        while(left <=right){
+            int mid = left +(right-left)/2;
+            // searching mid, left and right halves respectively
+            if (arr[mid]==target)
+                return mid;
+            if (arr[mid]<target)
+                left = mid+1;
+            else
+                right = mid-1;
+        }
+        return -1;
+    }
+    // Example usage
+    static void Main(){
+        int[] sortedArray={10,4,6,8,11,12,14};
+        int target = 10;
+
+        int result = BinaryS(sortedArray,target); // calling the binary search function
+        
+        // Output the result
+        if (result != -1)
+            Console.WriteLine($"Element {target} found at index {result}");
+        else
+            Console.WriteLine($"Element {target} not found in array");
+    }
+}
